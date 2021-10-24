71c4548ea2c7515665ee500b43da9e4942c9c4d8
MD JASHIMUDDIN <jash7818@gmail.com>
2021-10-24 08:12:25 +0530

    jashii96/future-computer

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ function isEmail($email) {
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 $first_name     = $_POST['first_name'];
 $last_name     = $_POST['last_name'];
-$email    = $_POST['company'];
+$email    = $_POST['email'];
 $phone    = $_POST['phone'];
 $comments = $_POST['comments'];
 
